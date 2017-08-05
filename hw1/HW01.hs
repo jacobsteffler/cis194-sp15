60af2534ce7c917aacdf67e2a522da4f650cd1fb
Jacob Steffler <jacobsteffler@gmail.com>
2017-08-05 11:46:40 -0500

    Initial.

---
+++ hw1/HW01.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit n = mod n 10
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = div n 10
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits n
+    | n <= 0 = []
+    | otherwise = lastDigit n : toRevDigits (dropLastDigit n)
+
+-- Exercise 3 -----------------------------------------
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther (x:y:xs) = x : (2*y) : doubleEveryOther xs
+
+-- Exercise 4 -----------------------------------------
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum (map sumOne xs)
+
+sumOne :: Integer -> Integer
+sumOne n = lastDigit n + dropLastDigit n
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn n = sumDigits (doubleEveryOther (toRevDigits n)) `mod` 10 == 0
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 s _ d = [(s,d)]
+hanoi n s t d =
