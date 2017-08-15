b8440e5655930d7b78ee77ef226959dad60d509d
Jacob Steffler <jacobsteffler@gmail.com>
2017-08-15 14:19:12 -0500

    Start HW4.

---
+++ hw4/HW04.hs
@@ -0,0 +1,68 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW04 where
+
+newtype Poly a = P [a]
+
+-- Exercise 1 -----------------------------------------
+
+x :: Num a => Poly a
+x = P [0, 1]
+
+-- Exercise 2 ----------------------------------------
+
+instance (Num a, Eq a) => Eq (Poly a) where
+    (==) (P c1) (P c2)
+        | l1 < l2   = (take l1 c2) == c1 && tailZero l1 c2
+        | l1 > l2   = (take l2 c1) == c2 && tailZero l2 c1
+        | otherwise = c1 == c2
+        where
+            l1 = length c1
+            l2 = length c2
+            tailZero n cs =
+                null $ filter (/= 0) (drop n cs)
+ 
+-- Exercise 3 -----------------------------------------
+
+instance (Num a, Eq a, Show a) => Show (Poly a) where
+    show (P coeffs) = case coeffs of
+        []      -> "0"
+        (c:[])  -> show c
+
+-- Exercise 4 -----------------------------------------
+
+plus :: Num a => Poly a -> Poly a -> Poly a
+plus = undefined
+
+-- Exercise 5 -----------------------------------------
+
+times :: Num a => Poly a -> Poly a -> Poly a
+times = undefined
+
+-- Exercise 6 -----------------------------------------
+
+instance Num a => Num (Poly a) where
+    (+) = plus
+    (*) = times
+    negate      = undefined
+    fromInteger = undefined
+    -- No meaningful definitions exist
+    abs    = undefined
+    signum = undefined
+
+-- Exercise 7 -----------------------------------------
+
+applyP :: Num a => Poly a -> a -> a
+applyP = undefined
+
+-- Exercise 8 -----------------------------------------
+
+class Num a => Differentiable a where
+    deriv  :: a -> a
+    nderiv :: Int -> a -> a
+    nderiv = undefined
+
+-- Exercise 9 -----------------------------------------
+
+instance Num a => Differentiable (Poly a) where
+    deriv = undefined
+
